d989af3f3bb3619d64e581c2ebbedafb11241d23
Bruno Wego <brunowego@gmail.com>
2016-03-22 19:31:52 -0300

    chore(npm): add sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 
-gulp.task('serve', function () {
+gulp.task('serve', ['sass'], function () {
   browserSync.init({
     server: {
-      baseDir: ['./docs', './src'],
+      baseDir: ['./docs', './src', './tmp'],
       routes: {
         '/node_modules': 'node_modules',
       },
     },
   });
+
+  gulp.watch('src/scripts/*.js').on('change', browserSync.reload);
+  gulp.watch('src/styles/*.scss', ['sass']);
+  gulp.watch('docs/*.html').on('change', browserSync.reload);
 });
+
+gulp.task('sass', function () {
+  return gulp.src('src/styles/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('tmp/styles'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
