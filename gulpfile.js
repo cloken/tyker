58e724bba3c596de55a0a135e7ddcef1ab187db1
Bruno Wego <brunowego@gmail.com>
2016-03-22 19:22:09 -0300

    refactor(project): add styles and scripts

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var browserSync = require('browser-sync').create();
 gulp.task('serve', function () {
   browserSync.init({
     server: {
-      baseDir: './',
+      baseDir: ['./docs', './src'],
     },
   });
 });
